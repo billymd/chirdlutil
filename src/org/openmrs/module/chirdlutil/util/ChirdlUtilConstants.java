53946c753a93015fca548b1ea1a0a2cbe52dfba8
McKee, Steven Jay <sjmckee@iu.edu>
2014-08-14 13:24:01 +0000

    CHICA-343: Filter printer locations by user on manual checkin.

---
+++ src/org/openmrs/module/chirdlutil/util/ChirdlUtilConstants.java
@@ -0,0 +1,41 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.chirdlutil.util;
+
+
+/**
+ * Constants class for dependent modules
+ * 
+ * @author Steve McKee
+ */
+public final class ChirdlUtilConstants {
+	
+	/*
+	 * User property constants
+	 */
+	public static final String USER_PROPERTY_LOCATION = "location";
+	
+	public static final String USER_PROPERTY_LOCATION_TAGS = "locationTags";
+	/*
+	 * 
+	 */
+	
+	/*
+	 * Location tag attribute constants
+	 */
+	public static final String LOC_TAG_ATTR_ACTIVE_PRINTER_STATION = "ActivePrinterStation";
+	/*
+	 * 
+	 */
+}
