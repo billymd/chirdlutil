4f9317e1b8c073b4c10fa0168d1253e7b47d2e1d
sjmckee <sjmckee@iu.edu>
2015-02-05 15:15:20 -0500

    Added new tool to print a file to a printer.

---
+++ src/org/openmrs/module/chirdlutil/tools/PrintFile.java
@@ -0,0 +1,122 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.chirdlutil.tools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.print.Doc;
+import javax.print.DocFlavor;
+import javax.print.DocPrintJob;
+import javax.print.PrintException;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import javax.print.SimpleDoc;
+import javax.print.attribute.HashPrintServiceAttributeSet;
+import javax.print.attribute.PrintServiceAttributeSet;
+import javax.print.attribute.standard.PrinterName;
+
+
+/**
+ * Utility used to print a file to a specific printer.
+ * 
+ * @author Steve McKee
+ */
+public class PrintFile {
+	
+	/**
+	 * Prints the specified file to the specified printer.  This method does perform any rendering for specialized file formats.  
+	 * Please test this method with your file type and printer.
+	 * 
+	 * @param printerName The name of the printer to send the print job.
+	 * @param fileToPrint The file to print.
+	 * @throws IOException
+	 * @throws PrintException
+	 */
+    public boolean printFile(String printerName, File fileToPrint) throws IOException, PrintException {
+    	if (printerName == null || printerName.trim().length() == 0) {
+    		System.err.println("A valid printerName parameter was not provided: " + printerName);
+    		return false;
+    	} else if (fileToPrint == null || !fileToPrint.exists() || !fileToPrint.canRead()) {
+    		System.err.println("A valid fileToPrint parameter was not provided: " + fileToPrint);
+    		return false;
+    	}
+    	
+    	PrintServiceAttributeSet printServiceAttributeSet = new HashPrintServiceAttributeSet();
+        printServiceAttributeSet.add(new PrinterName(printerName, null)); 
+        PrintService[] printServices = PrintServiceLookup.lookupPrintServices(DocFlavor.SERVICE_FORMATTED.PAGEABLE, printServiceAttributeSet);
+        if (printServices == null || printServices.length == 0) {
+        	System.err.println("No printers found for " + printerName);
+        	return false;
+        }
+        
+        PrintService selectedService = printServices[0];
+        FileInputStream psStream = null;  
+        psStream = new FileInputStream(fileToPrint);  
+        
+        DocPrintJob printJob = selectedService.createPrintJob();
+        DocFlavor flavor = DocFlavor.INPUT_STREAM.AUTOSENSE;
+        Doc document = new SimpleDoc(psStream, flavor, null);
+        printJob.print(document, null);
+        return true;
+    }
+	
+	/**
+	 * Prints the usage for this command line tool.
+	 */
+	private void printUsage() {
+		System.out.println("Arguments:");
+		System.out.println("Arg 1: The file to print");
+		System.out.println("Arg 2: The name of the printer");
+		System.out.println("");
+		System.out.println("Usage:");
+		System.out.println("PrintFile C:\\temp\\test.pdf Clinic_Printer");
+		System.out.println("This will print the test.pdf file to the Clinic_Printer printer");
+	}
+	
+	/**
+	 * Main method
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PrintFile printFile = new PrintFile();
+		if (args.length != 2) {
+			printFile.printUsage();
+			System.exit(1);
+		}
+		
+		String fileStr = args[0];
+		String printerName = args[1];
+		try {
+	        boolean completed = printFile.printFile(printerName, new File(fileStr));
+	        if (!completed) {
+	        	System.exit(1);
+	        }
+        }
+        catch (IOException e) {
+	        e.printStackTrace();
+	        System.exit(1);
+        }
+        catch (PrintException e) {
+	        e.printStackTrace();
+	        System.exit(1);
+        }
+		
+		System.exit(0);
+	}
+	
+	
+}
